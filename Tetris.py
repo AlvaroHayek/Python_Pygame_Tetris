7bd97c8c2de75b1bd978c6b752575caf38614f33
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-05-07 00:25:45 +0200

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 
@@ -6,3 +6,11 @@ screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update()
