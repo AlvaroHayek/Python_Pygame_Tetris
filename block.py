e138b684657094c0514a89cff2fe18cc2422d204
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-05-08 18:13:02 +0200

    more

--- block.py
+++ block.py
@@ -1,7 +1,13 @@
+from colors import Colors  # type: ignore
+
 class Block:
     def __init__(self, id):
         self.id = id
         self.cells = {}
         self.cell_size = 30
         self.rotation_state = 0
+        self.colors = Colors.get_cell_colors()
+        
+    def draw(self, screen):
+        
         
