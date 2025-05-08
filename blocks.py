902203902646e2185c5677ed60d8f729d6f5b6b3
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-05-08 18:25:13 +0200

    blocks

---
+++ blocks.py
@@ -0,0 +1,12 @@
+from block import Block # type: ignore
+from position import Position # type: ignore
+
+class LBlock(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
+            3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
+        }
