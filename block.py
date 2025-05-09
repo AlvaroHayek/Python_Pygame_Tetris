2a56fc3b476943a9614be8da96b3c3efd39c41f3
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-05-09 14:16:19 +0200

    Update block.py

--- block.py
+++ block.py
@@ -1,5 +1,6 @@
 from colors import Colors  # type: ignore
 import pygame
+from position import Position # type: ignore
 
 class Block:
     def __init__(self, id):
@@ -17,10 +18,16 @@ class Block:
         
     def get_cell_positions(self):
         tiles = self.cells[self.rotation_state]
-        
-    
+        moved_tiles = []
+        for position in tiles:
+            position = Position(position.row + self.row_offset, position.column + self.column_offset)
+            moved_tiles.append(position)
+        return moved_tiles
+            
+            
     def draw(self, screen):
-        tiles = self.cells[self.rotation_state]
+        #tiles = self.cells[self.rotation_state]
+        tiles = self.get_cell_positions()
         for tile in tiles:
             tile_rect = pygame.Rect(tile.column * self.cell_size + 1, tile.row * self.cell_size + 1,
             self.cell_size - 1, self.cell_size - 1)
